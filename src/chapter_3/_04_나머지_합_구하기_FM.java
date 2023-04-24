feb4f3a58ea4b6024ed74f8c3a177ecef27f1aab
dongso <ehdth5806@naver.com>
2023-04-24 16:03:27 +0900

    코딩테스트 여섯 번째 수업 commit.

--- src/chapter_3/_04_나머지_합_구하기_FM.java
+++ src/chapter_3/_04_나머지_합_구하기_FM.java
@@ -1,13 +1,13 @@
 package chapter_3;
 
-import java.io .BufferedReader;  
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
-public class Main {
+public class _04_나머지_합_구하기_FM {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
