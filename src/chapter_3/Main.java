aad7b1476f3b095542cd5062b545d4f9238c2ab1
dongso <ehdth5806@naver.com>
2023-05-15 16:52:52 +0900

    코딩테스트 열 두번째 수업 commit.

--- src/chapter_3/Main.java
+++ src/chapter_3/Main.java
@@ -1,57 +1,42 @@
 package chapter_3;
 
-import java.io .BufferedReader;    
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.Deque;
-import java.util.LinkedList;
-import java.util.StringTokenizer;
+import java.util.Scanner;
+import java.util.Stack;
 
 public class Main {
 	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int N = Integer.parseInt(st.nextToken());
-		int L = Integer.parseInt(st.nextToken());
-		
-		st = new StringTokenizer(br.readLine());
-	    Deque<Node> mydeque = new LinkedList<>();
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int[] A = new int[N];
 		for(int i = 0; i < N; i++) {
-			// 새로운 값이 들어올 때마다 정렬 대신 현재 수보다 큰 값을 덱에서 제거해 시간 복잡도를 줄임
-			int now = Integer.parseInt(st.nextToken());
-			
-			// mydeque가 비어있지 않고, mydeque의 마지막 값이 새로들어온 값보다 크다면
-			while(!mydeque.isEmpty() && mydeque.getLast().value > now) {
-				// 최소값을 구해줘야 하기 떄문에 마지막 값 삭제
-				mydeque.removeLast();
-			}
-			
-			// 마지막에 현재 입력받은 최소값과 인덱스 저장
-			mydeque.addLast(new Node(now, i));
-			
-			// 범위에서 벗어난 값은 덱에서 제거
-			// mydeque에 남아있는 첫번째 값의 인덱스가 현재 인덱스 - L값보다 작으면 L의 범위를 벗어났기 때문에 삭제
-			if(mydeque.getFirst().index <= i - L) {
-				mydeque.removeFirst();
-			}
-			bw.write(mydeque.getFirst().value + " ");
-		} // for문 끝.
-		bw.flush();
-		bw.close();
-	}
-	
-	static class Node {
-		public int value;
-		public int index;
-		
-		Node(int value, int index) {
-			this.value = value;
-			this.index = index;
+			A[i] = sc.nextInt();
 		}
+		Stack<Integer> stack = new Stack<>();
+		StringBuffer bf = new StringBuffer();
+		int num = 1; // 오름차순 수
+		boolean result = true;
+		for (int i = 0; i < A.length; i++) {
+			int su = A[i]; // 현재 수열의 수
+			if(su >= num) { // 현재 수열 값 >= 오름차순 자연수: 값이 같아질 때까지 puch() 수행
+				while(su >= num) {
+					stack.push(num++);
+					bf.append("+\n");
+				} // while문 끝.
+				stack.pop();
+				bf.append("-\n");
+			} else { // 현재 수열 값 < 오름차순 자연수: pop()을 수행해 수열 원소를 꺼냄
+				int n = stack.pop();
+				// 스택의 가장 위의 수가 만들어야 하는 수열의 수보다 크면 수열을 출력할 수 없음
+				if(n > su) {
+					System.out.println("NO");
+					result = false;
+					break;
+				} else {
+					bf.append("-\n");
+				}
+			}
+		} // for문 끝.
+		if(result) System.out.println(bf.toString());	
 	}
 }
--- src/chapter_3/_10_스택으로_오름차순_수열_만들기_FM.java
+++ src/chapter_3/_10_스택으로_오름차순_수열_만들기_FM.java
@@ -1,10 +1,42 @@
 package chapter_3;
 
+import java.util.Scanner;
+import java.util.Stack;
+
 public class _10_스택으로_오름차순_수열_만들기_FM {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int[] A = new int[N];
+		for(int i = 0; i < N; i++) {
+			A[i] = sc.nextInt();
+		}
+		Stack<Integer> stack = new Stack<>();
+		StringBuffer bf = new StringBuffer();
+		int num = 1; // 오름차순 수
+		boolean result = true;
+		for (int i = 0; i < A.length; i++) {
+			int su = A[i]; // 현재 수열의 수
+			if(su >= num) { // 현재 수열 값 >= 오름차순 자연수: 값이 같아질 때까지 puch() 수행
+				while(su >= num) {
+					stack.push(num++);
+					bf.append("+\n");
+				} // while문 끝.
+				stack.pop();
+				bf.append("-\n");
+			} else { // 현재 수열 값 < 오름차순 자연수: pop()을 수행해 수열 원소를 꺼냄
+				int n = stack.pop();
+				// 스택의 가장 위의 수가 만들어야 하는 수열의 수보다 크면 수열을 출력할 수 없음
+				if(n > su) {
+					System.out.println("NO");
+					result = false;
+					break;
+				} else {
+					bf.append("-\n");
+				}
+			}
+		} // for문 끝.
+		if(result) System.out.println(bf.toString());	
 	}
-
 }
