0c9b5f4b5b120cf6daf744f526a8f21ff7b17056
dongso <ehdth5806@naver.com>
2023-06-20 18:55:30 +0900

    코딩테스트 15번째 수업 commit.

--- src/chapter_3/_13_절대갑_힙_구현하기_FM.java
+++ src/chapter_3/_13_절대갑_힙_구현하기_FM.java
@@ -1,13 +1,13 @@
 package chapter_3;
 
-import java.io.BufferedReader;   
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.PriorityQueue;
 
-public class Main {
-	
-	public static void main(String[] args) throws IOException {
+public class _13_절대갑_힙_구현하기_FM {
+
+	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
 		PriorityQueue<Integer> myQueue = new PriorityQueue<Integer>((o1,o2) -> {
---
+++ src/chapter_3/_13_절대갑_힙_구현하기_dongso.java
@@ -0,0 +1,8 @@
+package chapter_3;
+
+public class _13_절대갑_힙_구현하기_dongso {
+
+	public static void main(String[] args) {
+		
+	}
+}
